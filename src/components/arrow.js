3b83754aa029be9087c4e621e3462203c98d7b01
Kayden Forthman <forthmankayden@gmail.com>
2018-11-27 00:22:08 -0700

    rendering arrow direction

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
- class Arrow extends Component {
-     constructor(props) {
+ 
+class Arrow extends Component {
+
+    constructor(props) {
         super(props)
-         this.state = {
+
+        this.state = {
             status: false
         }
-    }
-     toggleArrow = function() {
-        if(this.state.status) {
-            // close it
-            document.getElementById('arrow').classList.remove('arrow-closed');
-        } else {
-            // open it
-            document.getElementById('arrow').classList.add('arrow-closed');
-        }
-         this.setState({ status: !this.state.status })
-    }.bind(this);
-     render() {
-        return (
-            <a id="arrow" onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
-        )
-    }
+   }
+    toggleArrow = function() {
+       this.props.callback(this.state.status)
+       if(this.state.status) {
+          
+           document.getElementById(this.id).classList.remove('arrow-closed');
+       } else {
+         
+           document.getElementById(this.id).classList.add('arrow-closed');
+       }
+        this.setState({ status: !this.state.status })
+   }.bind(this);
+    render() {
+       this.id = `arrow-${this.props.id}`
+       return (
+         
+           <a id={this.id} onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+       )
+   }
 }
-
 export default Arrow;
