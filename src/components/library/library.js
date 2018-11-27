9da5128cfc5859cb25144b8e491b0b3db9b7a272
Kayden Forthman <forthmankayden@gmail.com>
2018-11-26 22:00:45 -0700

    mapped data to schedule component to only render courses user is enrolled in

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
- import * as actions from '../../actions';
+ 
+
+import * as actions from '../../actions';
  import LibraryCourse from './libraryCourse';
  class Library extends Component {
      componentWillMount() {
