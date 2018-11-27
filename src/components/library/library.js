52367c6e91c5c78b528e51096975a8f73d1af641
Kayden Forthman <forthmankayden@gmail.com>
2018-11-26 21:51:17 -0700

    mapped course object data in library

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
     renderCourses() {
         const data = this.props.courses
         return data.map((course, index) => {
-            return <LibraryCourse key={index}/>
+            return <LibraryCourse {...course} key={index}/>
         })
     }
      render() {
